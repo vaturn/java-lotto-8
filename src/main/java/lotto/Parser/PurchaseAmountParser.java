9d1632f7a519f452dc6958f5dcde19759ed83f7e
vaturn <will6867@naver.com>
2025-11-03 17:56:08 +0900

    feat: 우승 번호 파싱 기능 추가

--- src/main/java/lotto/Parser/PurchaseAmountParser.java
+++ src/main/java/lotto/Parser/PurchaseAmountParser.java
@@ -4,10 +4,6 @@ public class PurchaseAmountParser implements Parser<Integer>{
 
     @Override
     public Integer parse(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
+        return parseToInt(input);
     }
 }
--- src/main/java/lotto/Parser/WinningNumberParser.java
+++ src/main/java/lotto/Parser/WinningNumberParser.java
@@ -1,4 +1,23 @@
 package lotto.Parser;
 
-public class WinningNumberParser {
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningNumberParser implements Parser<List<Integer>>{
+
+    @Override
+    public List<Integer> parse(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] parts = input.split(",");
+        List<Integer> result = new ArrayList<>();
+
+        for(String part: parts){
+            result.add(parseToInt(part));
+        }
+
+        return result;
+    }
 }
