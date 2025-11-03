d1c741c8ebc6b7d70a9c8f0666107a139206c4ef
vaturn <will6867@naver.com>
2025-11-03 17:05:25 +0900

    refactor: View 기능만(입력, 출력) 수행할 수 있도록 수정

--- src/main/java/lotto/InputConsole/PurchaseAmountView.java
+++ src/main/java/lotto/InputConsole/PurchaseAmountView.java
@@ -1,19 +1,12 @@
 package lotto.InputConsole;
 
-import java.util.List;
 
 public class PurchaseAmountView extends ConsoleView{
     private static final String PROMPT_BEFORE_PURCHASE_INPUT = "구입금액을 입력해 주세요.";
-    public int getPurchaseAmount(){
-        return parsePurchase(getInput());
-    }
 
     @Override
     protected void printPromptBeforeInput() {
         System.out.println(PROMPT_BEFORE_PURCHASE_INPUT);
     }
 
-    private int parsePurchase(String userInput){
-        return Integer.parseInt(userInput);
-    };
 }
